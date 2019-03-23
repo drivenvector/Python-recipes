8f26f031708a26de98a70d0bd72def48e0295ae4
Saurabh Jain <28838017+drivenvector@users.noreply.github.com>
2019-03-23 08:39:20 +0530

    Created file classification_metrics
    This function returns performance metrics  based on confusion matrix. need to write the return part as per need

---
+++ def_classification_metrics.py
@@ -0,0 +1,34 @@
+def get_performance_metrics():
+    
+    """
+    Takes the predicted y by the model and returns several performance metrics of classification
+    Source: https://stackoverflow.com/questions/50666091/true-positive-rate-and-false-positive-rate-tpr-fpr-for-multi-class-data-in-py#answer-50671617
+    Usage: get_performance_metrics(y_pred)
+    """
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    FP = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix)  
+    FN = cnf_matrix.sum(axis=1) - np.diag(cnf_matrix)
+    TP = np.diag(cnf_matrix)
+    TN = cnf_matrix.sum() - (FP + FN + TP)
+
+    FP = FP.astype(float)
+    FN = FN.astype(float)
+    TP = TP.astype(float)
+    TN = TN.astype(float)
+
+    # Sensitivity, hit rate, recall, or true positive rate
+    TPR = TP/(TP+FN)
+    # Specificity or true negative rate
+    TNR = TN/(TN+FP) 
+    # Precision or positive predictive value
+    PPV = TP/(TP+FP)
+    # Negative predictive value
+    NPV = TN/(TN+FN)
+    # Fall out or false positive rate
+    FPR = FP/(FP+TN)
+    # False negative rate
+    FNR = FN/(TP+FN)
+    # False discovery rate
+    FDR = FP/(TP+FP)
+    # Overall accuracy
+    ACC = (TP+TN)/(TP+FP+FN+TN)
